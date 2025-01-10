32bd09f641c05fff9135b9475893434f7f99eafb
Kayky Ribeiro <142861320+KaykyRibeiroBarreto@users.noreply.github.com>
2025-01-10 13:06:42 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Logo from './components/logo/index';
+import './App.css'
+
+function App() {
+  return (
+    
+      <div className='App'>
+        <header className='App-header'>
+           <Logo />
+          </header>
+      </div>
+    
+  )
+}
+
+export default App
