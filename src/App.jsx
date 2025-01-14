285fed17decb77fb1ee5c40b6c1df7f44139833f
Kayky Ribeiro <142861320+KaykyRibeiroBarreto@users.noreply.github.com>
2025-01-13 21:53:32 -0300

    style(ui): adiciona nova fonte e atualiza gradiente de fundo do App

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import Logo from './components/logo/index';
-import './App.css'
+import './App.css';
+import User from './assets/user.png';
+import Cart from './assets/cart.png';
+
+const textoOpcoes = ['CATEGORIAS', 'FAVORITOS', 'MINHA ESTANTE']
+const icones = [User, Cart ]
 
 function App() {
   return (
@@ -7,6 +12,21 @@ function App() {
       <div className='App'>
         <header className='App-header'>
            <Logo />
+           <ul className='opcoes'>
+           {textoOpcoes.map((opcao, index) => (
+            <li className='opcao' key={index}>
+               <p>{opcao}</p>
+           </li>
+           ))}    
+           </ul>
+
+           <ul className='icones'>
+           {icones.map((icone, index) => (
+            <li className='icone' key={index}>
+               <img className='icone' src={icone} alt='icone' />
+           </li>
+           ))}    
+           </ul>
           </header>
       </div>
     
